3ff5bbac7fcef9309b3d14fc68bb45ccd4d0b60c
Johannes Bertens <johannes.bertens@live.nl>
2016-01-31 21:36:05 +0100

    Fist commit

---
+++ ContactList/Controllers/ContactsController.cs
@@ -0,0 +1,49 @@
+﻿using ContactList.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Linq;
+using Swashbuckle.Swagger.Annotations;
+using System.Net;
+
+namespace ContactList.Controllers
+{
+    public class ContactsController : ApiController
+    {
+        private const string Filename = "contacts.json";
+        private readonly GenericStorage storage;
+
+        public ContactsController()
+        {
+            this.storage = new GenericStorage();
+        }
+
+        private async Task<IEnumerable<Contact>> GetContacts()
+        {
+            var contacts = await this.storage.Get(Filename);
+            if (contacts != null) return contacts;
+
+            await this.storage.Save(new Contact[]{}, Filename);
+            return await this.storage.Get(Filename);
+        }
+
+        /// <summary>
+        /// Creates a new contact
+        /// </summary>
+        /// <param name="contact">The new contact</param>
+        /// <returns>The saved contact</returns>
+        [HttpPost]
+        [SwaggerResponse(HttpStatusCode.Created,
+            Description = "Created",
+            Type = typeof(Contact))]
+        [Route("~/contacts")]
+        public async Task<Contact> Post([FromBody] Contact contact)
+        {
+            var contacts = await this.GetContacts();
+            var contactList = contacts.ToList();
+            contactList.Add(contact);
+            await this.storage.Save(contactList, Filename);
+            return contact;
+        }
+    }
+}
---
+++ ContactList/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ContactList.Models
+{
+    public class Contact
+    {
+        /// <summary>
+        /// The Contact's full name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The Contact's email address. 
+        /// </summary>
+        public string EmailAddress { get; set; }
+    }
+}
